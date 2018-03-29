560fc1249992c6fc0dcdee622ed44584ed6f53f1
Jacquelyn M Santos <js92852@srcs.loc>
2018-03-29 12:26:22 -0700

    get items to show up

--- dynamic_list.js
+++ dynamic_list.js
@@ -1,9 +1,13 @@
 var button = document.getElementById('inputItem');
 var ul = document.getElementsByTagName('ul');
 
-button.addEventListener('click', runresponse);
+
 
 function runresponse() {
     var storeinput = document.getElementById('inputitem').value;
-
+    var item = document.createElement('li');
+    item.textContent = storeinput;
+    ul.appendChild(item)
 }
+
+button.addEventListener('click', runresponse);
