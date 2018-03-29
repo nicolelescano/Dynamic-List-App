01a1071b695faf828b039650e4cb846805c85e4d
Jacquelyn M Santos <js92852@srcs.loc>
2018-03-29 11:50:58 -0700

    all I've written

---
+++ dynamic_list.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('inputItem');
+var ul = document.getElementsByTagName('ul');
+
+button.addEventListener('click', runresponse);
+
+function runresponse() {
+    var storeinput = document.getElementById('inputitem').value;
+
+}
