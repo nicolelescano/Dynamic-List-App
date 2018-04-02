986eb7076f97ea350911c98d9a9d78ec5bfbc2cd
Jacquelyn M Santos <js92852@srcs.loc>
2018-04-02 12:24:17 -0700

    js

--- dynamic_list.js
+++ dynamic_list.js
@@ -1,13 +1,13 @@
-var button = document.getElementById('inputItem');
-var ul = document.getElementsByTagName('ul');
+var button = document.getElementById('inputItem'); //the add item button
+var ul = document.getElementById('dots'); //the list
 
 
-
-function runresponse() {
+function runresponse() { //creates the list
     var storeinput = document.getElementById('inputitem').value;
     var item = document.createElement('li');
     item.textContent = storeinput;
-    ul.appendChild(item)
-}
+    ul.appendChild(item);
+
+
 
 button.addEventListener('click', runresponse);
